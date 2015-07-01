d00ae6e09282a95ea81812a959877a647f4250cc
ewanbarr <ewan.d.barr@googlemail.com>
2015-07-01 13:53:03 +1000

    completed new coordinates module

--- anansi/tcc/coordinates.py
+++ anansi/tcc/coordinates.py
@@ -2,7 +2,7 @@ import warnings
 import ephem as eph
 import numpy as np
 from anansi.utils import d2r,r2d
-from anansi.coords import nsew_to_hadec,hadec_to_nsew
+from anansi.coords import nsew_to_hadec,hadec_to_nsew,azel_to_nsew,nsew_to_azel
 import os
 from ConfigParser import ConfigParser
 
@@ -13,15 +13,6 @@ MOL_LAT = config.getfloat("MolongloEphemeris","latitude")
 MOL_LON = config.getfloat("MolongloEphemeris","longitude")
 MOL_ELV = config.getfloat("MolongloEphemeris","elevation")
 MOL_HOR = config.getfloat("MolongloEphemeris","horizon")
-J2000 = eph.J2000
-
-FIXED_SYSTEMS = [
-    "equatorial_ha",
-    "nsew",
-    "horizontal"
-    ]
-
-# COMPLETE JUST EQUATORIAL TRACKING FOR TESTING PURPOSES
 
 class Molonglo(eph.Observer):
     def __init__(self,date=None,epoch=None):
@@ -30,108 +21,13 @@ class Molonglo(eph.Observer):
             self.epoch = epoch
         if date is not None:
             self.date = date
-        self.lat       = MOL_LAT
-        self.long      = MOL_LON
+        self.lat       = d2r(MOL_LAT)
+        self.long      = d2r(MOL_LON)
         self.elevation = MOL_ELV
-        self.horizon   = MOL_HOR
+        self.horizon   = d2r(MOL_HOR)
         self.compute_pressure()
 
 
-class Coordinates(eph.FixedBody):
-    def __init__(self,x,y,system="equatorial",units="radians",
-                 name="unknown",epoch="2000",date=None):
-        super(Coordinates,self).__init__()
-        self.name = name
-        self.system = system
-        x,y = self._fix_units(x,y,units)
-        self.x = x
-        self.y = y
-        self.date = eph.now()
-        self._epoch = epoch
-        self.update(date=self.date)
-
-    def _fix_units(self,x,y,units):
-        if units == "degrees":
-            x = d2r(x)
-            y = d2r(y)
-        elif units == "hhmmss":
-            if self.system in ["equatorial_ha","equatorial"]:
-                x = eph.hours(x)
-            else:
-                x = eph.degrees(x)
-            y = eph.degrees(y)
-        return x,y
-
-    def update(self,date=None):
-        """Convert all moving systems to equatorial"""
-        
-        if self.system in FIXED_SYSTEMS:
-            self._epoch = eph.now()
-            date = self.date
-            
-        self.observatory = Molonglo(date=date,epoch=self._epoch)
-        self.lst = self.observatory.sidereal_time()
-        
-        if self.system == "equatorial":
-            coords = eph.Equatorial(self.x,self.y)
-            self._ra  = coords.ra
-            self._dec = coords.dec
-
-        elif self.system == "galactic":
-            coords = eph.Equatorial(eph.Galactic(self.x,self.y))
-            self._ra  = coords.ra
-            self._dec = coords.dec
-
-        elif self.system == "equatorial_ha":
-            coords = eph.Equatorial(self.x,self.y)
-            ha = coords.ra
-            self._ra  = self.lst - ha
-            self._dec = coords.dec
-
-        elif self.system == "horizontal":
-            self._ra,self._dec = self.observatory.radec_of(self.x,self.y)
-
-        elif self.system == "nsew":
-            ha,dec = nsew_to_hadec(self.x,self.y)
-            coords = eph.Equatorial(ha,dec)
-            self.observatory = Molonglo(date,self.epoch)
-            self._ra  = self.observatory.sidereal_time() - ha
-            self._dec = coords.dec
-
-        self.compute(self.observatory)
-        gal = eph.Galactic(eph.Equatorial(self.a_ra,self.a_dec))
-        self.glat = gal.lat
-        self.glon = gal.long
-        self.ha = self.lst - self.a_ra
-        self.ns,self.ew = hadec_to_nsew(self.ha,self.a_dec)
-
-    def is_up(self):
-        self.update()
-        return self.alt > self.observatory.horizon
-
-    def _check_fixed(self):
-        if self.system in FIXED_SYSTEMS:
-            if not self.is_up():
-                raise eph.NeverUpError("Source never above horizon")
-            else:
-                raise eph.AlwaysUpError("Source never below horizon")
-
-    def next_rise(self):
-        self._check_fixed()
-        self.update()
-        return self.observatory.next_rising(self)
-
-    def next_set(self):
-        self._check_fixed()
-        self.update()
-        return self.observatory.next_setting(self)
-    
-    def next_transit(self):
-        self._check_fixed()
-        self.update()
-        return self.observatory.next_transit(self)
-
-    
 class CoordinatesMixin(object):
     def generate_other_systems(self):
         eq = eph.Equatorial(self.a_ra,self.a_dec,epoch=self._epoch)
@@ -146,32 +42,32 @@ class CoordinatesMixin(object):
         self.compute(date)
         return self.alt > MOL_HOR
         
-        
-class EquatorialCoordinates(eph.FixedBody,CoordinatesMixin):
-    def __init__(self,ra,dec,epoch=J2000):
+
+class RADecCoordinates(eph.FixedBody,CoordinatesMixin):
+    def __init__(self,ra,dec,epoch=eph.J2000):
         self._ra = ra
         self._dec = dec
         self._epoch = epoch
         eph.FixedBody.__init__(self)
-
+        
     def compute(self,date=None):
         date = eph.now() if date is None else date
         most = Molonglo(date,self._epoch)
         eph.FixedBody.compute(self,most)
         self.lst = most.sidereal_time()
-        self.ha = self.lst - self.a_ra
-        self.ns,self.ew = hadec_to_nsew(self.ha,self.a_dec)
+        self.ha = self.lst - self.ra
+        self.ns,self.ew = hadec_to_nsew(self.ha,self.dec)
         self.generate_other_systems()
-        
     
-class FixedCoordinates(eph.FixedBody,CoordinatesMixin):
-    def __init__(self,ns,ew,epoch=J2000):
+
+class NSEWCoordinates(eph.FixedBody,CoordinatesMixin):
+    def __init__(self,ns,ew):
         self.ns = ns
         self.ew = ew
-        self._epoch = epoch
         eph.FixedBody.__init__(self)
 
     def compute(self,date=None):
+        self._epoch = eph.now()
         date = eph.now() if date is None else date
         most = Molonglo(date,self._epoch)
         self.lst = most.sidereal_time()
@@ -179,11 +75,11 @@ class FixedCoordinates(eph.FixedBody,CoordinatesMixin):
         self._ra = self.lst - self.ha
         eph.FixedBody.compute(self,most)
         self.generate_other_systems()
-        
-        
+
+
 class BodyCoordinates(eph.FixedBody,CoordinatesMixin):
     def __init__(self,body,epoch=eph.J2000):
-        self.body = body
+        self.body = body()
         self.body._epoch = epoch
         self._epoch = epoch
         eph.FixedBody.__init__(self)
@@ -198,12 +94,66 @@ class BodyCoordinates(eph.FixedBody,CoordinatesMixin):
         self.ha = self.lst - self.a_ra
         self.ns,self.ew = hadec_to_nsew(self.ha,self.a_dec)
         self.generate_other_systems()
+
+
+def make_coordinates(x,y,system="equatorial",units="hhmmss",epoch="J2000"):
+    system = system.lower()
+    units = units.lower()
+    epoch = epoch.upper()
+    
+    try:
+        epoch = getattr(eph,epoch)
+    except AttributeError as error:
+        try:
+            epoch = eph.date(epoch)
+        except Exception as error:
+            raise Exception("Invalid epoch: %s"%epoch)
+    
+    try:
+        if units == "degrees":
+            x = eph.degrees(d2r(x))
+            y = eph.degrees(d2r(y))
+        elif units == "hhmmss":
+            if self.system in ["equatorial_ha","equatorial"]:
+                x = eph.hours(x)
+            else:
+                x = eph.degrees(x)
+                y = eph.degrees(y)
+    except Exception as error:
+        raise Exception("Invalid coordinates: %s, %s"%(x,y))
+
+    if system in ["equatorial","radec"]:
+        return RADecCoordinates(x,y,epoch)
+
+    elif system in ["equatorial_ha","hadec"]:
+        ns,ew = hadec_to_nsew(x,y)
+        return NSEWCoordinates(ns,ew)
+    
+    elif system == "nsew":
+        return NSEWCoordinates(x,y)
+    
+    elif system in ["horizontal","azel"]:
+        ns,ew = azel_to_nsew(x,y)
+        return NSEWCoordinates(ns,ew)
+
+    elif system in ["galactic","glgb"]:
+        eq = eph.Equatorial(eph.Galactic(x,y))
+        return RADecCoordinates(eq.ra,eq.dec,epoch)
         
+    else:
+        raise Exception("Unknown coordinate system: %s"%system)
+    
 
-
-
-
-
+def make_body(name,epoch="J2000"):
+    name = name.capitalize()
+    epoch = epoch.upper()
+    try:
+        epoch = getattr(eph,epoch)
+    except AttributeError as error:
+        raise Exception("Invalid epoch: %s"%epoch)
+    body = getattr(eph,name)
+    return BodyCoordinates(body,epoch)
+    
 
 def get_nsew(a_dec,ha):
     ew = np.arcsin((0.9999940546 * np.cos(a_dec) * np.sin(ha))
